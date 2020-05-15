e1ba079b0a8e671bfa43e898ec48f51546e3b858
LuisM <lumiroga@gmail.com>
2020-05-14 22:31:21 -0400

    Last try from tflearn to Keras

--- Neural Network/cat_or_dog/cat_or_dog.py
+++ Neural Network/cat_or_dog/cat_or_dog.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
 import os         
-from random import shuffle 
+from random import shuffle
 from tqdm import tqdm      
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import matplotlib.pyplot as plt
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Flatten,MaxPooling2D, Input, Dropout
+from keras.optimizers import Adam
+#import tflearn
+#from tflearn.layers.conv import conv_2d, max_pool_2d
+#from tflearn.layers.core import input_data, dropout, fully_connected
+#from tflearn.layers.estimator import regression
 
-tf.disable_v2_behavior()
+#tf.disable_v2_behavior()
 
 TRAIN_DIR = './train/train'
 TEST_DIR = './test1/test1'
@@ -50,16 +53,75 @@ def create_test_data():
     np.save('test_data.npy', testing_data)
     return testing_data
 
+np_load_old = np.load
 
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 # If dataset is not created:
-train_data = create_train_data()
-test_data = create_test_data()
+#train_data = create_train_data()
+#test_data = create_test_data()
 # If you have already created the dataset:
-# train_data = np.load('train_data.npy')
-# test_data = np.load('test_data.npy')
+train_data = np.load('train_data.npy')
+test_data = np.load('test_data.npy')
+
 train = train_data[:-500]
 test = train_data[-500:]
 X_train = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-y_train = [i[1] for i in train]
+y_train = np.array([i[1] for i in train])
 X_test = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-y_test = [i[1] for i in test]
+y_test = np.array([i[1] for i in test])
+###############################
+####### KERAS EXAMPLE #########
+###############################
+
+#tf.reset_default_graph()
+#create model
+#model = Sequential()
+#add model layers
+#model.add(Conv2D(64, kernel_size=3, activation=’relu’, input_shape=(28,28,1)))
+#model.add(Conv2D(32, kernel_size=3, activation=’relu’))
+#model.add(Flatten())
+#model.add(Dense(10, activation=’softmax’))
+#compile model using accuracy to measure model performance
+#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#train the model
+#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
+#predict first 4 images in the test set
+#model.predict(X_test[:4])
+###############################
+########## FROM TFLEARN TO KERAS ############
+###############################
+
+model = Sequential()
+
+model.add(Conv2D(32,strides=5,kernel_size=(IMG_SIZE,IMG_SIZE),activation='relu',input_shape=(IMG_SIZE, IMG_SIZE, 1)))
+model.add(MaxPooling2D(strides=5, dim_ordering="th"))
+model.add(Conv2D(64,strides=5, kernel_size=(IMG_SIZE,IMG_SIZE),activation='relu' ))
+model.add(MaxPooling2D(strides=5, dim_ordering="th"))
+model.add(Dense(1024, activation="relu"))
+model.add(Dropout(rate=0.8))
+model.add(Dense(2, activation="relu")) # input_shape=(24500,2
+
+model.compile(optimizer=Adam(lr=LR),loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10)
+
+###############################
+########## TFLEARN ############
+###############################
+"""
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input') 
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+model = tflearn.DNN(convnet, tensorboard_dir='log', tensorboard_verbose=0)
+model.fit({'input': X_train}, {'targets': y_train}, n_epoch=10, 
+          validation_set=({'input': X_test}, {'targets': y_test}), 
+          snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+"""
+###########################################################
